b8500fa087a19f569f373441efdd0240e325b0ca
slothlord15 <nelsonadika125@gmail.com>
2023-09-01 11:09:49 -0500

    Update ExamplePython.py
    added jobs

--- ExamplePython.py
+++ ExamplePython.py
@@ -7,3 +7,8 @@ y = "string 4"
 print(f"x = x {x} and y = {y}")
 
 print("c = {c} and d = {d}".format(c=1, d=2))
+
+job_list = {'john': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
+
+for name, job in job_list.items():
+    print(f"{name} is a {job}")
