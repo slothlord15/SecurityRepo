42e15086584c6f037f20b5e92fca2f1262db1273
slothlord15 <nelsonadika125@gmail.com>
2023-09-01 11:13:14 -0500

    Update ExamplePython.py
    Testing_Password

--- ExamplePython.py
+++ ExamplePython.py
@@ -1,3 +1,6 @@
+def test_password(x):
+    print(x)
+
 a = "string 1"
 b = "string 2"
 print("a =" + a + " and b=" + b)
@@ -12,3 +15,6 @@ job_list = {'john': 'Doctor', 'Jane': 'Engineer', 'Jim': 'Teacher'}
 
 for name, job in job_list.items():
     print(f"{name} is a {job}")
+
+Password = 'SuperSecretAdminPass'
+test_password(Password)
